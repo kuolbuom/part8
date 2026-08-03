6b39c069db0f85686c6c65109a2c3213921f9a30
kuol buom <kuolbuom111@gmail.com>
2026-08-03 13:34:35 +0300

    backend refactored

---
+++ fs-graphql/library-backend/schema.js
@@ -0,0 +1,41 @@
+const typeDefs = `
+  type Book {
+   title: String!
+   published: Int!
+   author: Author!
+   id: ID!
+   genres: [String!]!
+  }
+
+  type Author {
+   name: String!
+   id: ID!
+   born: Int
+   bookCount: Int!
+  }
+
+  type Query {
+    bookCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    findBook(title: String!): Book
+
+    authorCount: Int!
+    allAuthors: [Author!]!
+    findAuthor(name: String!):Author
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book!
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ):Author
+  }
+`;
+
+module.exports = typeDefs;
---
+++ fs-graphql/library-backend/server.js
@@ -0,0 +1,21 @@
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v4: uuid } = require("uuid");
+
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+
+const startServer = (port) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  startStandaloneServer(server, {
+    listen: { port: 4000 },
+  }).then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+  });
+};
+
+module.exports = startServer;
