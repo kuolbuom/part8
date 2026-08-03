39bbf80e444be97cdc365bab80d09f1a7d90f7be
kuol buom <kuolbuom111@gmail.com>
2026-08-03 16:21:13 +0300

    Complete the program so that database validation errors (e.g. book title or author name being too short) are handled sensibly. This means that they cause GraphQLError(opens in a new tab) with a suitable error message to be thrown.

--- fs-graphql/library-backend/resolvers.js
+++ fs-graphql/library-backend/resolvers.js
@@ -1,3 +1,5 @@
+const { GraphQLError } = require("graphql");
+
 const Book = require("./models/books");
 const Author = require("./models/authors");
 
@@ -169,32 +171,43 @@ const resolvers = {
 
   Mutation: {
     addBook: async (root, args) => {
-      //query from database
-      let author = await Author.findOne({
-        name: args.author,
-      });
-
-      //if author does not exist
-      if (!author) {
-        author = new Author({
+      try {
+        //query from database
+        let author = await Author.findOne({
           name: args.author,
         });
 
-        await author.save();
+        //if author does not exist
+        if (!author) {
+          author = new Author({
+            name: args.author,
+          });
+
+          await author.save();
+        }
+
+        //create a book
+        const book = new Book({
+          title: args.title,
+          published: args.published,
+          genres: args.genres,
+          author: author._id,
+        });
+
+        await book.save();
+
+        return book.populate("author");
+        //   if (!author) {
+      } catch (error) {
+        throw new GraphQLError(error.message, {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args,
+            error,
+          },
+        });
       }
 
-      //create a book
-      const book = new Book({
-        title: args.title,
-        published: args.published,
-        genres: args.genres,
-        author: author._id,
-      });
-
-      await book.save();
-
-      return book.populate("author");
-      //   if (!author) {
       //     authors = authors.concat({
       //       name: args.author,
       //       id: uuid(),
@@ -216,19 +229,29 @@ const resolvers = {
       //   return updatedAuthor;
     },
     editAuthor: async (root, args) => {
-      const author = await Author.findOne({
-        name: args.name,
-      });
+      try {
+        const author = await Author.findOne({
+          name: args.name,
+        });
 
-      if (!author) {
-        return null;
+        if (!author) {
+          return null;
+        }
+
+        author.born = args.setBornTo;
+
+        await author.save();
+
+        return author;
+      } catch (error) {
+        throw new GraphQLError(error.message, {
+          extensions: {
+            code: "BAD_USER_INPUT",
+            invalidArgs: args,
+            error,
+          },
+        });
       }
-
-      author.born = args.setBornTo;
-
-      await author.save();
-
-      return author;
     },
   },
 };
